daf0bbee552b4adf543849a473b031a793da0333
matthha <122128508+matthha@users.noreply.github.com>
2023-03-28 17:08:07 -0400

    Changed Speed Implementation

--- js/video.js
+++ js/video.js
@@ -2,7 +2,6 @@ var video = document.querySelector(".video");
 let speed = 1;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window");
 	video.autoplay = false;
 	video.loop = false
 	video.load()
@@ -20,13 +19,11 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 document.querySelector("#slower").addEventListener("click", function() {
-	speed *= 0.95;
-	video.playbackRate = speed;
+	video.playbackRate *= 0.95;
 	console.log(video.playbackRate)
 });
 document.querySelector("#faster").addEventListener("click", function() {
-	speed *= 1.05;
-	video.playbackRate = speed;
+	video.playbackRate *= 1.05;
 	console.log(video.playbackRate)
 });
 document.querySelector("#skip").addEventListener("click", function() {
